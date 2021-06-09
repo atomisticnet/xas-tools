a471cebb79c99316e7b06ca92b0ee04381e3e0c9
Nongnuch Artrith <nartrith@atomistic.net>
2021-06-09 02:23:52 -0400

    Basic broadening

--- xas_tools/spectra.py
+++ xas_tools/spectra.py
@@ -15,6 +15,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from .util import broaden
+
 __author__ = "Alex Urban, Nong Artrith"
 __email__ = "a.urban@columbia.edu"
 __maintainer__ = "Nong Artrith, Alexander Urban"
@@ -35,6 +37,7 @@ class AbsorptionSpectrum(object):
         for csv_path in self.metadata['spectrum_path']:
             self.raw_data.append(pd.read_csv(os.path.join(path, csv_path)))
         self.E_min, self.E_max = self._get_edge_energies()
+        self.broadened = None
 
     def __str__(self):
         out = 'Absorption Spectrum\n'
@@ -51,7 +54,42 @@ class AbsorptionSpectrum(object):
             E_max = max(E_max, np.max(E))
         return E_min, E_max
 
-    def plot_atomic_lines(self, dE=2.0, **kwargs):
+    @property
+    def spectrum(self):
+        if self.broadened is None:
+            return None
+        else:
+            X, Y = self.broadened[0]
+            for b in self.broadened[1:]:
+                Y += b[1]
+            return X, Y
+
+    def calculated_broadened(self, gauss_fwhm, lorentz_fwhm1,
+                             lorentz_fwhm2, n=100, energy_range=None,
+                             dE=1.0):
+        """
+        Calculate broadened atomic spectra and store them in the attribute
+        `self.broadened`.
+
+        """
+
+        self.broadened = []
+        E_range = [self.E_min - dE, self.E_max + dE]
+        if energy_range is not None:
+            if energy_range[0] is not None:
+                E_range[0] = max(E_range[0], energy_range[0])
+            if energy_range[1] is not None:
+                E_range[1] = min(E_range[1], energy_range[1])
+        for i, line in enumerate(self.raw_data):
+            m = self.multiplicity[i]
+            X = line['Aligned Energy (eV)'].values
+            Y = line['Intensity'].values*m
+            X2, Y2 = broaden(X, Y, gauss_fwhm, xlim=E_range, n=n)
+            X, Y = broaden(X2, Y2, lorentz_fwhm1, fwhm2=lorentz_fwhm2,
+                           xlim=E_range, n=n, lorentz=True)
+            self.broadened.append((X, Y))
+
+    def plot_atomic_lines(self, dE=1.0, **kwargs):
         """
         Plot the XAS lines for all individual atoms, weighted with their
         multiplicity.
@@ -88,6 +126,49 @@ class AbsorptionSpectrum(object):
             Y = line['Intensity']*m
             ax.plot(X, Y)
         ax.set_xlim([self.E_min - dE, self.E_max + dE])
+        ax.set_yticks([])
+        ax.set_xlabel("Energy (eV)")
+        ax.set_ylabel("Intensity")
+        return fig, ax
+
+    def plot_broadened(self, dE=1.0, **kwargs):
+        """
+        Plot the XAS line from the combination of all atoms after
+        broadening.  Broadened lines must first be calculated with the
+        method `self.calculated_broadened`.
+
+        Args:
+          dE (float): energy margin below and above lowest and highest
+            energy, respectively
+          **kwargs: Additional keyword arguments are used to update
+            matplotlib's rcparams.
+
+        Returns:
+          fig, ax - matplotlib figure and axis
+
+        """
+        if self.broadened is None:
+            print("First call `calculate_broadened()`")
+            return
+
+        rcparams = {"font.size": 14,
+                    "legend.frameon": False,
+                    "xtick.top": True,
+                    "xtick.direction": "in",
+                    "xtick.minor.visible": True,
+                    "xtick.major.size": 8,
+                    "xtick.minor.size": 4,
+                    "ytick.right": True,
+                    "ytick.direction": "in",
+                    "ytick.minor.visible": True,
+                    "ytick.major.size": 8,
+                    "ytick.minor.size": 4}
+        if kwargs:
+            rcparams.update(kwargs)
+        plt.rcParams.update(rcparams)
+        fig, ax = plt.subplots()
+        X, Y = self.spectrum
+        ax.plot(X, Y)
         ax.set_yticks([])
         ax.set_xlabel("Energy (eV)")
         ax.set_ylabel("Intensity")
---
+++ xas_tools/util.py
@@ -0,0 +1,100 @@
+"""
+Utility functions.
+
+"""
+
+import numpy as np
+
+__author__ = "Nong Artrith, Alexander Urban"
+__email__ = "nartrith@atomistic.net"
+__maintainer__ = "Nong Artrith, Alexander Urban"
+__maintainer_email__ = ("nartrith@atomistic.net"
+                        + ", aurban@atomistic.net")
+__date__ = "2021-06-08"
+__version__ = "0.1"
+
+
+def gauss_function(x, x0, s):
+    """
+    Normalized Gaussian
+
+    Args:
+      x (float): argument
+      x0 (float): mean
+      s (float): standard deviation
+
+    """
+    return np.exp(-0.5*((x-x0)/s)**2)/(s*np.sqrt(2.0*np.pi))
+
+
+def lorentz_function(x, x0, w):
+    """
+    Normalized Lorentz (Cauchy) function
+
+    Args:
+      x (float): argument
+      x0 (float): center
+      w (float): full width at half maximum (FWHM)
+
+    """
+    return 2.0/(w*np.pi)/(1.0 + 4.0*(x - x0)**2/w**2)
+
+
+def broaden(x_in, y_in, fwhm, fwhm2=None, xlim=None, n=100, sigma=False,
+            lorentz=False):
+    """
+    Broaden a line shape via convolution with a distribution function.
+    A Gaussian function is used unless `lorentz` is set to True, in
+    which case a Lorentzian function is used.
+
+    Args:
+      x_in, y_in (ndarray): X and Y values of the line shape
+      fwhm (float): full width at half maximum (FWHM) of the distribution
+        function
+      fwhm2 (float): if specified, the FWHM is linearly varied from
+        `fwhm` to `fwhm2` from the lowest to the highest X value
+      xlim (tuple): Tuple (x0, x1) with the minimal and maximal X values
+      n (int): number of discretization points
+      sigma (bool): if True, assume that `fwhm` is the standard deviation
+      lorentz (bool): if True, perform convolution with a Lorentzian function
+        instead of a Gaussian function
+
+    Returns:
+      x, y (ndarray)
+
+    """
+
+    if fwhm2 is None:
+        scale = np.ones(n)
+    else:
+        scale = np.linspace(1.0, fwhm2/fwhm, n)
+
+    if lorentz:
+        func = lorentz_function
+        sigma = True
+    else:
+        func = gauss_function
+
+    if sigma or lorentz:
+        s = fwhm
+    else:
+        s = fwhm/(2.0*np.sqrt(2.0*np.log(2.0)))
+
+    x0 = np.min(x_in)
+    x1 = np.max(x_in)
+    if xlim is not None:
+        if xlim[0] is not None:
+            x0 = max(xlim[0], x0)
+        if xlim[1] is not None:
+            x1 = min(xlim[1], x1)
+
+    y = np.zeros(n)
+    x = np.linspace(x0, x1, n)
+    y[0] = y_in[0]
+    y[-1] = y_in[-1]
+    for i in range(1, len(x_in)-1):
+        dx = 0.5*(x_in[i+1]-x_in[i-1])
+        for j in range(1, n-1):
+            y[j] += y_in[i]*func(x[j], x_in[i], scale[j]*s)*dx
+
+    return x, y
